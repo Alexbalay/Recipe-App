95ab524657ae8c80ba1e615b4013d66fddb0e114
Alexbalay <alexbalay94@gmail.com>
2023-05-16 00:44:37 -0300

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+//Variables que should be const and reused throughout the project
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+//Timeout for timeout function in helpers.js
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
